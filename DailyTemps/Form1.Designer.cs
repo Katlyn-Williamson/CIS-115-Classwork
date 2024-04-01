4ad4c813e42e3038d1b58d9ce0b1075338d686a2
Katlyn Williamson <156861642+Katlyn-Williamson@users.noreply.github.com>
2024-04-01 17:03:12 -0400

    Add files via upload

---
+++ DailyTemps/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace DailyTemps
+{
+    partial class DailyTemps
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TempEntry = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(480, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Weekly Temperature Total:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(375, 82);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Temperature (Degrees Fahrenheit)";
+            // 
+            // TempEntry
+            // 
+            this.TempEntry.Location = new System.Drawing.Point(18, 98);
+            this.TempEntry.Name = "TempEntry";
+            this.TempEntry.Size = new System.Drawing.Size(179, 40);
+            this.TempEntry.TabIndex = 2;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(230, 98);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(179, 40);
+            this.btnEnter.TabIndex = 3;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Location = new System.Drawing.Point(509, 286);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(129, 33);
+            this.lblTotal.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 348);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(528, 40);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Weekly Temperature Average:";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.Location = new System.Drawing.Point(559, 348);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(129, 33);
+            this.lblAverage.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Copperplate Gothic Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(497, 9);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(309, 214);
+            this.listBox1.TabIndex = 6;
+            // 
+            // DailyTemps
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(21F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightYellow;
+            this.ClientSize = new System.Drawing.Size(818, 445);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.TempEntry);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Copperplate Gothic Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(10, 8, 10, 8);
+            this.Name = "DailyTemps";
+            this.Text = "DailyTemps";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TempEntry;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ DailyTemps/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DailyTemps
+{
+    public partial class DailyTemps : Form
+    {
+        int temp;
+        int count = 0;
+        int total = 0;
+        int average = 0;
+
+        public DailyTemps()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            
+
+            if (count < 7)
+            {
+                temp = Convert.ToInt32(TempEntry.Text);
+
+                if (temp < -20 || temp > 130)
+                {
+                    MessageBox.Show("The temperature that you entered does not fall in the acceptable range. Try again.");
+                }
+                else
+                {
+                    listBox1.Items.Add(temp);
+                    count++;
+                    total += temp;
+                    TempEntry.Clear();
+
+
+                }
+            }
+            else
+            {
+                temp = Convert.ToInt32(TempEntry.Text);
+                
+                average = total/7;
+                
+                btnEnter.Enabled = false;
+                TempEntry.Clear();
+                lblAverage.Text = "" + average;
+                lblTotal.Text = "" + total;
+                
+                
+                
+            }
+        }
+        
+    }
+}
