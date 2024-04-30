b8ff5cf07ef6764813c34198a256236f021ec042
Katlyn Williamson <156861642+Katlyn-Williamson@users.noreply.github.com>
2024-04-30 12:46:04 -0400

    Add files via upload

---
+++ PaintingEstimate/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace PaintingEstimate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.LengthEntry = new System.Windows.Forms.TextBox();
+            this.WidthEntry = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEstimate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Length >>";
+            // 
+            // LengthEntry
+            // 
+            this.LengthEntry.Location = new System.Drawing.Point(110, 79);
+            this.LengthEntry.Name = "LengthEntry";
+            this.LengthEntry.Size = new System.Drawing.Size(100, 22);
+            this.LengthEntry.TabIndex = 1;
+            // 
+            // WidthEntry
+            // 
+            this.WidthEntry.Location = new System.Drawing.Point(110, 121);
+            this.WidthEntry.Name = "WidthEntry";
+            this.WidthEntry.Size = new System.Drawing.Size(100, 22);
+            this.WidthEntry.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Width >>";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(233, 57);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Please enter the following information:";
+            // 
+            // btnEstimate
+            // 
+            this.btnEstimate.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEstimate.Location = new System.Drawing.Point(16, 157);
+            this.btnEstimate.Name = "btnEstimate";
+            this.btnEstimate.Size = new System.Drawing.Size(229, 23);
+            this.btnEstimate.TabIndex = 5;
+            this.btnEstimate.Text = "Estimate";
+            this.btnEstimate.UseVisualStyleBackColor = true;
+            this.btnEstimate.Click += new System.EventHandler(this.btnEstimate_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(283, 93);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(216, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ft";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(216, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "ft";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(311, 333);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnEstimate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.WidthEntry);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LengthEntry);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Painting Estimate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox LengthEntry;
+        private System.Windows.Forms.TextBox WidthEntry;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEstimate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ PaintingEstimate/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintingEstimate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SqFootage()
+        {
+            int Length = Convert.ToInt16(LengthEntry.Text);
+            int Width = Convert.ToInt16(WidthEntry.Text);
+
+            int SquareFeet = Length * Width;
+            int PricePerFoot = 6;
+            int TotalCost;
+
+            TotalCost = SquareFeet * PricePerFoot;
+
+            label4.Text = "Price: " + TotalCost.ToString("C");
+        }
+
+       
+
+        private void btnEstimate_Click(object sender, EventArgs e)
+        {
+            SqFootage();
+
+            
+        }
+    }
+}
