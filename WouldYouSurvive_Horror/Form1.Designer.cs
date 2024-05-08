30255b3f505962e4dd1703d3e6ef4d63c83c0477
Katlyn Williamson <156861642+Katlyn-Williamson@users.noreply.github.com>
2024-05-08 17:01:25 -0400

    Add files via upload

---
+++ WouldYouSurvive_Horror/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WouldYouSurvive_Horror
+{
+    partial class WouldYouSurviveInteractive
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WouldYouSurviveInteractive));
+            this.Title = new System.Windows.Forms.Label();
+            this.lblSQ = new System.Windows.Forms.Label();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.BackColor = System.Drawing.Color.Transparent;
+            this.Title.Font = new System.Drawing.Font("Chiller", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.Snow;
+            this.Title.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.Title.Location = new System.Drawing.Point(324, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(617, 71);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Would you Survive?";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblSQ
+            // 
+            this.lblSQ.Font = new System.Drawing.Font("Centaur", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSQ.ForeColor = System.Drawing.Color.Snow;
+            this.lblSQ.Location = new System.Drawing.Point(113, 80);
+            this.lblSQ.Name = "lblSQ";
+            this.lblSQ.Size = new System.Drawing.Size(1034, 308);
+            this.lblSQ.TabIndex = 1;
+            this.lblSQ.Text = "label1";
+            // 
+            // btnYes
+            // 
+            this.btnYes.BackColor = System.Drawing.Color.Red;
+            this.btnYes.ForeColor = System.Drawing.Color.Snow;
+            this.btnYes.Location = new System.Drawing.Point(119, 429);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(253, 65);
+            this.btnYes.TabIndex = 2;
+            this.btnYes.Text = "Yes";
+            this.btnYes.UseVisualStyleBackColor = false;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.BackColor = System.Drawing.Color.Red;
+            this.btnNo.ForeColor = System.Drawing.Color.Snow;
+            this.btnNo.Location = new System.Drawing.Point(894, 429);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(253, 65);
+            this.btnNo.TabIndex = 3;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = false;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.ForeColor = System.Drawing.Color.Snow;
+            this.button1.Location = new System.Drawing.Point(496, 558);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 65);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start Over";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // WouldYouSurviveInteractive
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 43F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1223, 635);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.lblSQ);
+            this.Controls.Add(this.Title);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Chiller", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 10, 8, 10);
+            this.Name = "WouldYouSurviveInteractive";
+            this.Text = "Would You Survive?";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label lblSQ;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WouldYouSurvive_Horror/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace WouldYouSurvive_Horror
+{
+    public partial class WouldYouSurviveInteractive : Form
+    {
+        
+        
+
+        // Start
+        public WouldYouSurviveInteractive()
+        {
+            InitializeComponent();
+            lblSQ.Text = "Welcome foolish mortal! Let us see if you will survive. Are you ready to begin?";
+            
+        }
+
+        // Death and Survival message methods
+        private void Death()
+        {
+            lblSQ.Text = "Dead As A Doornail. Too bad, you did not survive.";
+            button1.Visible = true;
+            btnNo.Visible = false; btnYes.Visible = false;
+        }
+        private void BarelySurvived()
+        {
+            lblSQ.Text = "Well... You made it. Barely. We will get you next time!";
+            button1.Visible = true;
+            btnNo.Visible = false; btnYes.Visible = false;
+        }
+        private void SurvivedInjured()
+        {
+            lblSQ.Text = "You may be injured, but you survived. Don't worry, the emotional trauma will heal in time. Maybe not as quick as your leg, but still.";
+            button1.Visible = true;
+            btnNo.Visible = false; btnYes.Visible = false;
+        }
+        private void Survived()
+        {
+            lblSQ.Text = "Congratulations! You have survived! You have what it takes to survive a Horrror situation.";
+            button1.Visible = true;
+            btnNo.Visible = false; btnYes.Visible = false;
+        }
+
+       
+
+        // If the user answers yes.
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            if (lblSQ.Text == "Welcome foolish mortal! Let us see if you will survive. Are you ready to begin?")
+            {
+                lblSQ.Text = "It is Halloween night, you and some friends decided to break into the old plantation house on Elm Street to tell scary stories in the dark. You and your friends gather in the old, run down sitting room. The only light in the room is coming from the moon shining through the busted window. Looking around, you notice an old fireplace. Your friends state they want to sit in the dark, but you want to start a fire. Do you sit in the dark?";
+            }
+
+           else if (lblSQ.Text == "It is Halloween night, you and some friends decided to break into the old plantation house on Elm Street to tell scary stories in the dark. You and your friends gather in the old, run down sitting room. The only light in the room is coming from the moon shining through the busted window. Looking around, you notice an old fireplace. Your friends state they want to sit in the dark, but you want to start a fire. Do you sit in the dark?")
+            {
+                Death();
+            }
+
+            else if (lblSQ.Text == "With the fire lit, you all gather in the center of the aging and rotting room. Forming a circle, you sit. You decide to tell the first story. You are just setting the mood of your story when you hear a knock from behind you. You pause. You shake it off, and continue. All of a sudden there is a loud crashing sound. You jump. Your friends want you to investigate. Do you investigate alone?")
+            {
+                Death();
+            }
+
+            else if (lblSQ.Text == "You and your friends head towards where the sound came from. You all are not entirely sure where it came from, so you and two others go off to the left, while the other three go to the right. You hear a scratching noise from behind a door. You open the door to find a set of stairs, and another door to your left. Do you go upstairs?")
+            {
+                lblSQ.Text = "You and one other friend slowly make your way upstairs. Your one friend took the door to the left. At the top of the stairs, you look around, noticing old furniture covered with sheets. You move further into the room. Looking around you see something tall towards the back corner covered with a sheet. It looks tall and wide enough to be human. Do you investigate the figure?";
+            }
+
+            else if (lblSQ.Text == "You and one other friend slowly make your way upstairs. Your one friend took the door to the left. At the top of the stairs, you look around, noticing old furniture covered with sheets. You move further into the room. Looking around you see something tall towards the back corner covered with a sheet. It looks tall and wide enough to be human. Do you investigate the figure?")
+            {
+                lblSQ.Text = "You move closer to the figure. You reach out to touch the sheet, but you hesitate. Did you just see it move? Your heart starts to pound. Do you remove the sheet?";
+            }
+
+            else if (lblSQ.Text == "You move closer to the figure. You reach out to touch the sheet, but you hesitate. Did you just see it move? Your heart starts to pound. Do you remove the sheet?")
+            {
+                Death();
+            }
+
+            else if (lblSQ.Text == "Your heart is pounding, you start to get nauseous. You and you friend decide to get out of this room as quick as possible. You turn to run. Running down the stairs you here a scream. You run faster. You run outside leaving everyone else inside. Do you continue running?")
+            {
+                Survived();
+            }
+        }
+
+        // If the user answers no.
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            if (lblSQ.Text == "Welcome foolish mortal! Let us see if you will survive. Are you ready to begin?")
+            {
+                Death();
+            }
+
+            else if (lblSQ.Text == "It is Halloween night, you and some friends decided to break into the old plantation house on Elm Street to tell scary stories in the dark. You and your friends gather in the old, run down sitting room. The only light in the room is coming from the moon shining through the busted window. Looking around, you notice an old fireplace. Your friends state they want to sit in the dark, but you want to start a fire. Do you sit in the dark?")
+            {
+                lblSQ.Text = "With the fire lit, you all gather in the center of the aging and rotting room. Forming a circle, you sit. You decide to tell the first story. You are just setting the mood of your story when you hear a knock from behind you. You pause. You shake it off, and continue. All of a sudden there is a loud crashing sound. You jump. Your friends want you to investigate. Do you investigate alone?";
+            }
+
+            else if (lblSQ.Text == "With the fire lit, you all gather in the center of the aging and rotting room. Forming a circle, you sit. You decide to tell the first story. You are just setting the mood of your story when you hear a knock from behind you. You pause. You shake it off, and continue. All of a sudden there is a loud crashing sound. You jump. Your friends want you to investigate. Do you investigate alone?")
+            {
+                lblSQ.Text = "You and your friends head towards where the sound came from. You all are not entirely sure where it came from, so you and two others go off to the left, while the other three go to the right. You hear a scratching noise from behind a door. You open the door to find a set of stairs, and another door to your left. Do you go upstairs?";
+            }
+
+            else if (lblSQ.Text == "You and your friends head towards where the sound came from. You all are not entirely sure where it came from, so you and two others go off to the left, while the other three go to the right. You hear a scratching noise from behind a door. You open the door to find a set of stairs, and another door to your left. Do you go upstairs?")
+            {
+                BarelySurvived();
+            }
+
+            else if (lblSQ.Text == "You and one other friend slowly make your way upstairs. Your one friend took the door to the left. At the top of the stairs, you look around, noticing old furniture covered with sheets. You move further into the room. Looking around you see something tall towards the back corner covered with a sheet. It looks tall and wide enough to be human. Do you investigate the figure?")
+            {
+                lblSQ.Text = "Your heart is pounding, you start to get nauseous. You and you friend decide to get out of this room as quick as possible. You turn to run. Running down the stairs you here a scream. You run faster. You run outside leaving everyone else inside. Do you continue running?";
+            }
+
+            else if (lblSQ.Text == "You move closer to the figure. You reach out to touch the sheet, but you hesitate. Did you just see it move? Your heart starts to pound. Do you remove the sheet?")
+            {
+                BarelySurvived();
+            }
+
+            else if (lblSQ.Text == "Your heart is pounding, you start to get nauseous. You and you friend decide to get out of this room as quick as possible. You turn to run. Running down the stairs you here a scream. You run faster. You run outside leaving everyone else inside. Do you continue running?")
+            {
+                SurvivedInjured();
+            }
+        }
+
+        // Restart button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblSQ.Text = "Welcome foolish mortal! Let us see if you will survive. Are you ready to begin?";
+            button1.Visible = false;
+            btnNo.Visible = true; btnYes.Visible = true;
+        }
+    }
+}
